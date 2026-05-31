1b022eb370cf610f3d870576356d1c9c65395607
ai-gdz <137825038+helpers-bot@users.noreply.github.com>
2026-05-31 22:29:47 +0300

    Update config.php

--- api/config.php
+++ api/config.php
@@ -1,6 +1,4 @@
 <?php
-// api/config.php
-// Ключи NOWPayments (доступны только серверу)
 define('NOWPAYMENTS_API_KEY', '2cf60623-8bb3-41bb-8a4b-07d205c38bfa');
 define('NOWPAYMENTS_IPN_SECRET', 'Jd9TSa3DLFxi2Hjs38iPpAq1AjSBcwZU');
 define('SUPABASE_URL', 'https://zvneiyhsoivaozzhzeoy.supabase.co');
