0dafe50437353099912f1a1e78bda319cb9f1a82
ai-gdz <137825038+helpers-bot@users.noreply.github.com>
2026-07-14 17:25:27 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 export const CONFIG = {
     supabase: {
-        url: 'https://l2ls0oS3ZwF9GUTochw_NQ.supabase.co',
+        url: 'https://l2ls0os3zwf9gutochw_nq.supabase.co',
         publishableKey: 'sb_publishable_l2ls0oS3ZwF9GUTochw_NQ_FKV4rF6Y'
     },
     cloudinary: {
-        cloudName: 'ЗАМЕНИТЕ_НА_ВАШ_CLOUD_NAME',
+        cloudName: 'ЗАМЕНИТЕ_НА_ВАШ',
         uploadPreset: 'vds_upload'
     },
     content: {
