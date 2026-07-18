88633d3210e78b0a43c4ecf7a1fc96d98d51082a
ai-gdz <137825038+helpers-bot@users.noreply.github.com>
2026-07-18 20:25:55 +0300

    Delete js directory

--- js/config.js
+++
@@ -1,6 +0,0 @@
-// Конфигурация Supabase
-const SUPABASE_URL = 'https://aywfviexlltujeoaqeaq.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_l2ls0oS3ZwF9GUTochw_NQ_FKV4rF6Y';
-const REDIRECT_URL = 'https://vds-game.ink';
-
-export { SUPABASE_URL, SUPABASE_KEY, REDIRECT_URL };
